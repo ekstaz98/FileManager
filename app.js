b289913539bdaa385660c3f4317971508eb3772a
ekstaz98 <igroook1z2x3c@mail.ru>
2020-02-22 13:46:49 +0700

    add cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -13,17 +13,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+//path.join(__dirname, 'public')
 
 app.use('/', indexRouter);
 app.use('/forward', indexRouter);
 app.use('/back', indexRouter);
 app.use('/delete', indexRouter);
 app.use('/upload', indexRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
